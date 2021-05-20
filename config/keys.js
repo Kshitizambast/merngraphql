1e2bc8b21d09f47d25aea6f861c21d0ceb5c1603
Kshitiz Ambast <kshitiz.k63@gmail.com>
2021-05-20 14:05:01 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,1 @@
-module.exports = {
-    MONGODB: 'mongodb+srv://user123:kLeEJ1UqL0ZBhZvX@mernshopping.mtded.mongodb.net/merng?retryWrites=true&w=majority',
-    SECRET_KEY: 'Some very secret key'
-}
+
